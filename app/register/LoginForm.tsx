fe9e9755b6acdf2a9bd1bee722b3775525d857ae
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-03-01 14:43:04 +0100

    Add minimal styling to register page

---
+++ app/register/LoginForm.tsx
@@ -0,0 +1,33 @@
+'use client';
+import { useState } from 'react';
+import styles from './page.module.scss';
+
+type Props = { returnTo?: string | string[] };
+
+export default function LoginForm() {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  return (
+    <main className={styles.main}>
+      <div className={styles.wrapper1}>
+        <h1>Register here</h1>
+        <form className={styles.form}>
+          <label>
+            <input
+              className={styles.input}
+              // onChange={(event) => setUsername(event.currentTarget.value)}
+            />
+            Username
+          </label>
+          <br />
+          <label>
+            <input className={styles.input} />
+            Password
+          </label>
+          <br />
+          <button className={styles.button}>Register</button>
+        </form>
+      </div>
+    </main>
+  );
+}
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,28 +1,7 @@
-'use client';
 import { useState } from 'react';
+import LoginForm from './LoginForm';
 import styles from './page.module.scss';
 
 export default function RegisterPage() {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
-  return (
-    <main className={styles.main}>
-      <div className={styles.wrapper1}>
-        <h1>Register here</h1>
-        <form>
-          <label>
-            <input
-            // onChange={(event) => setUsername(event.currentTarget.value)}
-            />
-            Username
-          </label>
-          <label>
-            <input />
-            Password
-          </label>
-          <button className={styles.button}>Register</button>
-        </form>
-      </div>
-    </main>
-  );
+  return <LoginForm />;
 }
