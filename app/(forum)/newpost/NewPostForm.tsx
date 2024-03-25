a480d263133103772b2b41ea218ab591d360db82
Overcook3265 <151878132+Overcook3265@users.noreply.github.com>
2024-03-25 14:54:36 +0100

    Add incomplete post redirect functionality

--- app/(forum)/newpost/NewPostForm.tsx
+++ app/(forum)/newpost/NewPostForm.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import styles from './page.module.scss';
 
@@ -9,34 +10,81 @@ export default function NewPostForm() {
   const [isOpChanged, setIsOpChanged] = useState(false);
   // const [postTimestamp, setPostTimestamp] = useState('');
   // careful: change useState if input is a number or boolean
+  const router = useRouter();
   const [rating, setRating] = useState(0);
   const [errors, setErrors] = useState<{ message: string }[]>([]);
+
+  // async function handlePost(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   // fetch method to post data to the server
+  //   // define the route where to post it
+  //   const response = await fetch('api/post', {
+  //     method: 'POST',
+  //     // content of the data
+  //     body: JSON.stringify({
+  //       postTitle,
+  //       postText,
+  //       isOpChanged,
+  //       // postTimestamp,
+  //       rating,
+  //     }),
+  //     // this is needed just in case a user has an old browser
+  //     // the content type is a json
+  //     headers: {
+  //       'Content-type': 'application.json',
+  //     },
+  //   });
+
+  //   const data: RegisterResponseBodyPost = await response.json();
+  //   if ('errors' in data) {
+  //     setErrors(data.errors);
+  //     console.log(errors);
+  //     return;
+  //   }
+  // }
+
+  // function onSubmit(event: FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   router.push('/forum');
+  //   router.refresh();
+  // }
 
   async function handlePost(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     // fetch method to post data to the server
     // define the route where to post it
-    const response = await fetch('api/post', {
-      method: 'POST',
-      // content of the data
-      body: JSON.stringify({
-        postTitle,
-        postText,
-        isOpChanged,
-        // postTimestamp,
-        rating,
-      }),
-      // this is needed just in case a user has an old browser
-      // the content type is a json
-      headers: {
-        'Content-type': 'application.json',
-      },
-    });
-
-    const data: RegisterResponseBodyPost = await response.json();
-    if ('errors' in data) {
-      setErrors(data.errors);
-      return;
+    try {
+      const response = await fetch('api/post', {
+        method: 'POST',
+        // content of the data
+        body: JSON.stringify({
+          postTitle,
+          postText,
+          isOpChanged,
+          // postTimestamp,
+          rating,
+        }),
+        // this is needed just in case a user has an old browser
+        // the content type is a json
+        headers: {
+          'Content-type': 'application.json',
+        },
+      });
+      if (!response.ok) {
+        throw new Error('Failed to submit the data. Please try again.');
+      } else {
+        router.push('/forum');
+        router.refresh();
+      }
+      const data: RegisterResponseBodyPost = await response.json();
+      if ('errors' in data) {
+        setErrors(data.errors);
+        console.log(errors);
+        return;
+      }
+    } catch (error) {
+      setErrors(error.message);
+      console.error(error);
     }
   }
 
@@ -53,6 +101,7 @@ export default function NewPostForm() {
               className={styles.inputtitle}
               onChange={(event) => setPostTitle(event.currentTarget.value)}
               placeholder="Title"
+              required
             />
             <br />
             Rate the encounter
@@ -65,6 +114,7 @@ export default function NewPostForm() {
                 onChange={(event) =>
                   setRating(Number(event.currentTarget.value))
                 }
+                required
               />
               /10
             </label>
@@ -86,10 +136,17 @@ export default function NewPostForm() {
                 // cols={100}
                 onChange={(event) => setPostText(event.currentTarget.value)}
                 placeholder="Write your story here"
+                required
               />
             </div>
             <br />
-            <button className={styles.button}>Submit</button>
+            <button
+              className={styles.button}
+              type="submit"
+              // onClick={onSubmit}
+            >
+              Submit
+            </button>
             {/* {errors.map((error) => (
             <div className="error" key={`error-${error.message}`}>
               <ErrorMessage>{error.message}</ErrorMessage>
